7620f7b0d68cd77064c3cf2b283e251262c21885
ddeemer <ddeemer@purdue.edu>
2025-09-18 15:34:47 -0400

    added pseudo functions for input validation

---
+++ scripts/get_input.py
@@ -0,0 +1,17 @@
+'''Figure out the input file to use and tell the user if they have an invalid file.
+'''
+
+def get_input_file():
+    '''Get the input file from the command line arguments or use a default file.
+    '''
+    pass
+
+def validate_input_file(file_path):
+    '''Check if the input file exists and is readable.
+    '''
+    pass
+
+def do_something_with_file(file_path):
+    '''Perform some operation with the input file.
+    '''
+    pass
